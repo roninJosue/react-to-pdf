3c109e9286a27179653c8195f0525f9a2a589b5b
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-03-13 20:02:22 -0600

    Complete functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import {PDFViewer} from '@react-pdf/renderer'
 import './App.css';
 import Invoice from "./components/reports/Invoice";
+import invoice from "./data/invoice";
 
 function App() {
   return (
     <>
       <PDFViewer width='1000' height='600' className='app'>
-        <Invoice />
+        <Invoice invoice={invoice} />
       </PDFViewer>
     </>
   );
--- src/components/reports/Invoice.js
+++ src/components/reports/Invoice.js
@@ -1,11 +1,43 @@
-import {Document, Page, View, Text} from "@react-pdf/renderer";
+import {
+  Document,
+  Page,
+  StyleSheet,
+  Image
+} from "@react-pdf/renderer";
+import logo from '../../logo.png'
+import InvoiceTitle from "./InvoiceTitle";
+import InvoiceNo from "./InvoiceNo";
+import BillTo from "./BillTo";
+import InvoiceItemsTable from "./InvoiceItemsTable";
+import InvoiceThankYouMsg from "./InvoiceThankYouMsg";
 
-const Invoice = () => (
+const styles = StyleSheet.create({
+  page: {
+    fontFamily: 'Helvetica',
+    fontSize: 11,
+    paddingTop: 30,
+    paddingLeft: 60,
+    paddingRight: 60,
+    lineHeight: 1.5,
+    flexDirection: 'column'
+  },
+  logo: {
+    width: 74,
+    height: 66,
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  }
+})
+
+const Invoice = ({invoice}) => (
   <Document>
-    <Page size='A4'>
-      <View>
-        <Text>Example</Text>
-      </View>
+    <Page size='A4' style={styles.page}>
+      <Image style={styles.logo} src={logo} />
+      <InvoiceTitle title='Invoice' />
+      <InvoiceNo invoice={invoice} />
+      <BillTo invoice={invoice} />
+      <InvoiceItemsTable invoice={invoice} />
+      <InvoiceThankYouMsg />
     </Page>
   </Document>
 )
